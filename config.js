ff4e0af4bae05792f8cb03ad7b3cfed70b12afa6
juanmagarcia-etti <enigmapayne@gmail.com>
2026-08-11 17:54:17 +0200

    Conectar Google Sheets como fuente de datos (CSV)

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Si csvUrl esta vacio, se usaran los discos de ejemplo (DATOS_EJEMPLO) mas abajo.
 
 const CONFIG = {
-  csvUrl: "",
+    csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4G8zbkwARA27RiZl9gEH4ALSbIwzTi8fZNLkYRiVH7l1LfMn1IvpD8dxu6NOsLP1PsthZRzJsi/pub?gid=0&single=true&output=csv",
   titulo: "La Estanteria"
 };
 
